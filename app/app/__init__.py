bf3d70db72d19bfe18baaa886b95b8ac3e17ac65
Di M <meerkdir@gmail.com>
2022-11-08 16:29:00 +0200

    test without celery

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,4 +1,4 @@
-from app.settings import *
-from .celery import app as celery_app
+# from app.settings import *
+# from .celery import app as celery_app
  
-__all__ = ['celery_app']
+# __all__ = ['celery_app']
